b34bccbcbc8c029b67d1350fd6794b29039990ac
Gabriellia Venegas-Johnson <61035529+venegasgabby76@users.noreply.github.com>
2021-03-01 19:13:15 -0800

    added depend and made sure that everything was hooked up properly

---
+++ gabby-venegas/src/App.js
@@ -0,0 +1,28 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <p>
+          this is just to make sure that all of my stuff is going to be working correctly on this
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
